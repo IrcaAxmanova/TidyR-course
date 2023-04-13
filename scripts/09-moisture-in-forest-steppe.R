8cff3b7b67d0e56d7b00a8fc30a83c41af6373c8
Kryshtoson <krystof.chytry@gmail.com>
2023-04-13 11:28:40 +0200

    update KC codes

--- scripts/09-moisture-in-forest-steppe.R
+++ scripts/09-moisture-in-forest-steppe.R
@@ -13,17 +13,9 @@ env <- read_csv(r'(data\Forest-steppe_KC\Chytry-Krystof_forest-steppe-v1_2021-05
 spe <- read_csv(r'(data\Forest-steppe_KC\Chytry-Krystof_forest-steppe-v1_2021-05-24_SPE.csv)')
 traits <- read_csv(r'(data\Forest-steppe_KC\Chytry-Krystof_forest-steppe-v1_2021-05-24_TRAITS.csv)')
 sites <- read_csv(r'(data\Forest-steppe_KC\Chytry-Krystof_forest-steppe-v1_2021-05-24_SITES.csv)')
-countries <- rnaturalearth::ne_countries(scale = 10, continent = 'Europe', returnclass = 'sf')
 
 sites |>
   st_as_sf(coords = c('LONGITUDE', 'LATITUDE'), crs = 4326) -> sites_sf
-
-bb <- st_bbox(sites_sf) |> as.numeric()
-sites_sf |>
-  ggplot() +
-  geom_sf(data = countries, fill = NA) +
-  geom_sf(aes(colour = PART_2), size = 4) +
-  coord_sf(xlim = bb[c(1, 3)], ylim = bb[c(2, 4)])
 
 chelsa01 <- rast(r'(data\Forest-steppe_KC\chelsa01.tif)')
 chelsa12 <- rast(r'(data\Forest-steppe_KC\chelsa12.tif)')
