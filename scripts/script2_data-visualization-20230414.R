389f0fcc3838d47c885d1305c90e39dad87f1cb6
Kryshtoson <krystof.chytry@gmail.com>
2023-04-14 12:26:09 +0200

    gapminder 1

--- scripts/script2_data-visualization-20230414.R
+++ scripts/script2_data-visualization-20230414.R
@@ -6,5 +6,26 @@ read_csv(r'(data\Gapminder\gapminder_data.csv)') |>
   ggplot(aes(children_per_woman, life_expectancy, text = country)) +
   geom_point(aes(fill = continent, size = pop), shape = 21) +
   scale_size_continuous(range = c(1, 15)) -> p
-
 ggplotly(p, tooltip = 'text')
+
+# 1:10 == c(5, 7)
+
+read_csv(r'(data\Gapminder\gapminder_data.csv)') |>
+  filter(year %in% c(1952, 2007)) |>
+  ggplot(aes(children_per_woman, life_expectancy, text = country)) +
+  geom_point(aes(fill = continent, size = pop), shape = 21) +
+  scale_size_continuous(range = c(1, 15)) +
+  facet_wrap(~year)
+
+hist(read_csv(r'(data\Gapminder\gapminder_data.csv)') $pop)
+
+read_csv(r'(data\Gapminder\gapminder_data.csv)') |>
+  filter(pop > 10000000) |>
+  mutate(child_survival = (1000-child_mortality)/1000) |>
+  select(country, continent, year, child_survival, gdpPercap) |>
+  mutate(gdpPercap = log10(gdpPercap)) |>
+  arrange(year) |>
+  ggplot(aes(gdpPercap, child_survival)) +
+  geom_path(aes(group = country, colour = continent), arrow = arrow()) +
+  facet_wrap(~continent)
+
