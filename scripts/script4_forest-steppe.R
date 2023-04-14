545c5ea4fb77d6b322deba7ca22682768b532ee8
Kryshtoson <krystof.chytry@gmail.com>
2023-04-14 16:34:17 +0200

    forest-steppe step 2

--- scripts/script4_forest-steppe.R
+++ scripts/script4_forest-steppe.R
@@ -1,3 +1,5 @@
+library(ggpubr)
+library(broom)
 library(terra)
 library(tidyverse)
 library(sf)
@@ -9,15 +11,15 @@ spe <- read_csv(r'(data\Forest-steppe_KC\Chytry-Krystof_forest-steppe-v1_2021-05
 traits <- read_csv(r'(data\Forest-steppe_KC\Chytry-Krystof_forest-steppe-v1_2021-05-24_TRAITS.csv)')
 sites_sf <- sites |> st_as_sf(coords = c('LONGITUDE', 'LATITUDE'), crs = 4326)
 countries <- ne_countries(returnclass = 'sf', scale = 'large')
-sites$precipitation <- terra::extract(rast(r'(data\Forest-steppe_KC\chelsa12.tif)'), vect(sites_sf))[,2]
+sites$precipitation <- terra::extract(rast(r'(data\Forest-steppe_KC\chelsa12.tif)'), vect(sites_sf))[, 2]
 
 #' plotting the extent of the dataset
 sites_sf |>
   ggplot() +
   geom_sf(data = countries) +
   geom_sf(size = 3, aes(colour = PART_2)) +
-  coord_sf(xlim = st_bbox(sites_sf)[c(1,3)],
-           ylim = st_bbox(sites_sf)[c(2,4)])
+  coord_sf(xlim = st_bbox(sites_sf)[c(1, 3)],
+           ylim = st_bbox(sites_sf)[c(2, 4)])
 
 #' presence absence species data
 spe |>
@@ -28,6 +30,19 @@ spe |>
   left_join(sites |> select(SITE_ID, precipitation)) |>
   select(-SITE_ID) |>
   group_by(name) |>
-  nest() -> df
-
-df$data[[2]]
+  nest() |>
+  mutate(no_occ = map_dbl(data, function(df){sum(df$value)})) |>
+  filter(no_occ > 5) |>
+  mutate(models = map(data, function(df) {
+    glm(value ~ precipitation, data = df, family = binomial())
+  }
+  )) |>
+  mutate(model_out = map(models, tidy)) |>
+  unnest(model_out) |>
+  filter(term == 'precipitation') |>
+  left_join(traits |> select(name = TAXON, PLANT_HEIGHT)) |>
+  filter(p.value < .1) |>
+  ggplot(aes(estimate, PLANT_HEIGHT)) +
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  stat_cor()
