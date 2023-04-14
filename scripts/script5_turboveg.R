58a025e79b5c19a3593fe3bd847ef7a34d3df6e1
Kryshtoson <krystof.chytry@gmail.com>
2023-04-14 16:47:00 +0200

    turboveg files

---
+++ scripts/script5_turboveg.R
@@ -0,0 +1,12 @@
+# Real world data from a database - challenge to prepare tidy data
+# data from a database export (Turboveg, Czech species list) [Real-worl-data_IA]
+# tvabund – species data in a long format, opened as dbf file from Turbowin/data/xx , species names as codes, cover as codes
+# tvhabita - header data,each releve has RELEVE_NR code, which matches the one in tvabund
+# species - species list, to translate the species codes to species names, stored in the Turbowin/species as species.dbf
+# nomenclature - alternative, file where the Turboveg codes are matched to more name variants/ Danihelka, Kaplan, ESy (file name: nomenclatureCZ-2023-04-02-IA)
+# cover - translation of cover codes to %, requires info on the cover-abundance scale (tvhabita) (file name: coverCZ-2023-04-07-IA)
+
+
+
+
+
