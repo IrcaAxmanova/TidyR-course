441f369b5b47f584912a397f63c13d9f38a33fdc
Kryshtoson <krystof.chytry@gmail.com>
2023-04-14 16:04:37 +0200

    forest-steppe step 1

--- scripts/script4_forest-steppe.R
+++ scripts/script4_forest-steppe.R
@@ -9,7 +9,9 @@ spe <- read_csv(r'(data\Forest-steppe_KC\Chytry-Krystof_forest-steppe-v1_2021-05
 traits <- read_csv(r'(data\Forest-steppe_KC\Chytry-Krystof_forest-steppe-v1_2021-05-24_TRAITS.csv)')
 sites_sf <- sites |> st_as_sf(coords = c('LONGITUDE', 'LATITUDE'), crs = 4326)
 countries <- ne_countries(returnclass = 'sf', scale = 'large')
+sites$precipitation <- terra::extract(rast(r'(data\Forest-steppe_KC\chelsa12.tif)'), vect(sites_sf))[,2]
 
+#' plotting the extent of the dataset
 sites_sf |>
   ggplot() +
   geom_sf(data = countries) +
@@ -17,14 +19,15 @@ sites_sf |>
   coord_sf(xlim = st_bbox(sites_sf)[c(1,3)],
            ylim = st_bbox(sites_sf)[c(2,4)])
 
-rast(r'(data\Forest-steppe_KC\chelsa12.tif)')
-
+#' presence absence species data
 spe |>
-  distinct(PLOT_ID, name = TAXON, value = 1) |>
+  left_join(env |> select(PLOT_ID, SITE_ID)) |>
+  distinct(SITE_ID, name = TAXON, value = 1) |>
   pivot_wider(values_fill = 0) |>
-  pivot_longer(-1)
+  pivot_longer(-1) |>
+  left_join(sites |> select(SITE_ID, precipitation)) |>
+  select(-SITE_ID) |>
+  group_by(name) |>
+  nest() -> df
 
-
-#' env |>
- #' select(PLOT_ID, SITE_ID, LONGITUDE, LATITUDE) |>
-  #' semi_join(sites |> filter(PART_2), by = 'SITE_ID')
+df$data[[2]]
